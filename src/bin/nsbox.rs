5a005a68648e0d4185440d9b09e3e234723e39d5
Nathan Gill <nathan.j.gill@outlook.com>
2025-09-06 22:41:35 +0100

    feat: add basic sandbox command

--- src/bin/nsbox.rs
+++ src/bin/nsbox.rs
@@ -1,5 +1,42 @@
 extern crate nsbox;
 
+use nix::unistd::execve;
+use nsbox::Sandbox;
+use std::env::{args, vars_os};
+use std::ffi::CString;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = args().collect();
+
+    if args.len() < 2 {
+        eprintln!("Usage: nsbox <COMMAND> [OPTIONS] ...");
+        std::process::exit(1);
+    }
+
+    let command = CString::new(args[1].clone()).unwrap();
+    let arguments: Vec<CString> = args[1..]
+        .iter()
+        .map(|arg| CString::new(arg.clone()).unwrap())
+        .collect();
+    let environment: Vec<CString> = vars_os()
+        .map(|(k, v)| {
+            let s = format!("{}={}", k.to_string_lossy(), v.to_string_lossy());
+            CString::new(s).unwrap()
+        })
+        .collect();
+
+    let mut sandbox = Sandbox::new(None);
+
+    let result = sandbox
+        .run(move || {
+            match execve(&command, &arguments, &environment) {
+                Ok(_) => {}
+                Err(e) => eprintln!("execve failed: {}", e),
+            }
+
+            1
+        })
+        .expect("Sandbox failed");
+
+    std::process::exit(result);
 }
